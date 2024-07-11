032d670d8470e71c3fdcd27b1d3ced51232a61bb
Dmitriy JavaLearn <koltsifer@gmail.com>
2024-07-11 21:18:40 +0300

    Fixed random range, and other cosmetic changes

--- src/main/java/ru/kolts/java/basic/homework/lesson3/MainApp.java
+++ src/main/java/ru/kolts/java/basic/homework/lesson3/MainApp.java
@@ -13,35 +13,30 @@ public class MainApp {
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
         Random random = new Random();
-        // random.nextInt(1 + 1) - 1
-        // Рандомный диапазон чисел от 1 до -1
 
         while (running) {
             System.out.println("\nВведите номер задания от 1 до 5 включительно. Для выхода введите x");
             String task = scanner.nextLine();
             switch (task) {
                 case "1":
-                    System.out.println("\nМетод 1:");
                     greetings();
                     break;
                 case "2":
-                    System.out.println("\nМетод 2:");
+                    // В аргументах рандомный диапазон чисел от 1 до -1
                     checkSign(
-                            random.nextInt(1 + 1) - 1,
-                            random.nextInt(1 + 1) - 1,
-                            random.nextInt(1 + 1) - 1
+                            random.nextInt((1 + 1) + 1) - 1,
+                            random.nextInt((1 + 1) + 1) - 1,
+                            random.nextInt((1 + 1) + 1) - 1
                     );
                     break;
                 case "3":
-                    System.out.println("\nМетод 3:");
                     selectColor();
                     break;
                 case "4":
-                    System.out.println("\nМетод 4:");
                     compareNumbers();
                     break;
                 case "5":
-                    System.out.println("\nМетод 5:");
+                    // В аргументах рандомный диапазон чисел от 10 до -10
                     addOrSubtractAndPrint(
                             random.nextInt(10 + 10) - 10,
                             random.nextInt(10 + 10) - 10,
@@ -65,6 +60,7 @@ public class MainApp {
     //    (1) Реализуйте метод greetings(), который при вызове должен
     //    отпечатать в столбец 4 слова: Hello, World, from, Java;
     public static void greetings() {
+        System.out.println("\nМетод 1:");
         System.out.println("Hello\nWorld\nfrom\nJava");
     }
 
@@ -73,7 +69,9 @@ public class MainApp {
     //    и если она больше или равна 0, то вывести в консоль сообщение “Сумма положительная”,
     //    в противном случае - “Сумма отрицательная”;
     public static void checkSign(int a, int b, int c) {
+        System.out.println("\nМетод 2:");
         System.out.println("a = " + a + " | b = " + b + " | c = " + c);
+
         if (a + b + c >= 0)
             System.out.println("Сумма положительная");
         else System.out.println("Сумма отрицательная");
@@ -84,6 +82,7 @@ public class MainApp {
     //    выведено сообщение “Красный”, если от 10 до 20 включительно, то “Желтый”,
     //    если больше 20 - “Зеленый”;
     public static void selectColor() {
+        System.out.println("\nМетод 3:");
         Random random = new Random();
         int data = random.nextInt(30);
 
@@ -98,11 +97,12 @@ public class MainApp {
     //    a и b с любыми начальными значениями. Если a больше или равно b,
     //    то необходимо вывести в консоль сообщение “a >= b”, в противном случае “a < b”;
     public static void compareNumbers() {
+        System.out.println("\nМетод 4:");
         Random random = new Random();
         int a = random.nextInt(100);
         int b = random.nextInt(100);
-
         System.out.println("a = " + a + " | b = " + b);
+
         if (a >= b)
             System.out.println("a >= b");
         else System.out.println("a < b");
@@ -112,6 +112,8 @@ public class MainApp {
     //    Если increment = true, то метод должен к initValue прибавить delta и отпечатать в
     //    консоль результат, в противном случае - вычесть;
     public static void addOrSubtractAndPrint(int initValue, int delta, boolean increment) {
+        System.out.println("\nМетод 5:");
+
         if (increment)
             System.out.println("Результат: " + initValue + " + " + delta + " = " + (initValue + delta));
         else System.out.println("Результат: " + initValue + " - " + delta + " = " + (initValue - delta));
