24127149895b752e4a6f5a24aee43441fc5d19d8
Dmitriy JavaLearn <koltsifer@gmail.com>
2024-08-22 14:21:44 +0300

    Homework 9 lesson 15 complete

--- src/main/java/ru/kolts/java/basic/homewrok9/lesson15/collections/Employee.java
+++ src/main/java/ru/kolts/java/basic/homewrok9/lesson15/collections/Employee.java
@@ -1,6 +1,57 @@
 package ru.kolts.java.basic.homewrok9.lesson15.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Employee {
-    private String name;
-    private int age;
+    private final String name;
+    private final int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static List<String> returnOnlyNamesList(List<Employee> list) {
+        List<String> newList = new ArrayList<>();
+        for (Employee employee : list) {
+            newList.add(employee.name);
+        }
+        return newList;
+    }
+
+    public static List<Employee> trimEmployeeListByAge(List<Employee> list, int minAge) {
+        List<Employee> newList = new ArrayList<>();
+        for (Employee employee : list) {
+            if (employee.age >= minAge) {
+                newList.add(employee);
+            }
+        }
+        return newList;
+    }
+
+    public static boolean isAgeAverageInList(List<Employee> list, int averageAge) {
+        int average = 0;
+        for (Employee employee : list) {
+            average += employee.age;
+        }
+        return average / list.size() > averageAge;
+    }
+
+    public static Employee returnYoungest(List<Employee> list) {
+        int smallestAge = Integer.MAX_VALUE;
+        Employee youngest = null;
+        for (Employee employee : list) {
+            if (employee.age < smallestAge) {
+                smallestAge = employee.age;
+                youngest = employee;
+            }
+        }
+        return youngest;
+    }
+
+    @Override
+    public String toString() {
+        return "Сотрудник: Имя - \"" + name + "\" | Возраст - " + age;
+    }
 }
--- src/main/java/ru/kolts/java/basic/homewrok9/lesson15/collections/MainApp.java
+++ src/main/java/ru/kolts/java/basic/homewrok9/lesson15/collections/MainApp.java
@@ -1,29 +1,27 @@
 package ru.kolts.java.basic.homewrok9.lesson15.collections;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
 /**
  * Реализуйте метод, принимающий в качестве аргументов числа min и max, и возвращающий ArrayList с набором
- * последовательных значений в указанном диапазоне (min и max включительно, шаг - 1);
+ * последовательных значений в указанном диапазоне (min и max включительно, шаг - 1);<p>
  * Реализуйте метод, принимающий в качестве аргумента список целых чисел, суммирующий все элементы, значение
- * которых больше 5, и возвращающий сумму;
+ * которых больше 5, и возвращающий сумму;<p>
  * Реализуйте метод, принимающий в качестве аргументов целое число и ссылку на список, метод должен переписать
- * каждую заполненную ячейку списка указанным числом;
+ * каждую заполненную ячейку списка указанным числом;<p>
  * Реализуйте метод, принимающий в качестве аргументов целое число и ссылку на список, увеличивающий каждый
- * элемент списка на указанное число;
- * Создайте класс Сотрудник с полями: имя, возраст;
- * Реализуйте метод, принимающий в качестве аргумента список сотрудников, и возвращающий список их имен;
+ * элемент списка на указанное число;<p>
+ * Создайте класс Сотрудник с полями: имя, возраст;<p>
+ * Реализуйте метод, принимающий в качестве аргумента список сотрудников, и возвращающий список их имен;<p>
  * Реализуйте метод, принимающий в качестве аргумента список сотрудников и минимальный возраст, и возвращающий
- * список сотрудников, возраст которых больше либо равен указанному аргументу;
+ * список сотрудников, возраст которых больше либо равен указанному аргументу;<p>
  * Реализуйте метод, принимающий в качестве аргумента список сотрудников и минимальный средний возраст,
- * и проверяющий что средний возраст сотрудников превышает указанный аргумент;
+ * и проверяющий что средний возраст сотрудников превышает указанный аргумент;<p>
  * Реализуйте метод, принимающий в качестве аргумента список сотрудников, и возвращающий ссылку на самого
  * молодого сотрудника.
  */
-
 public class MainApp {
-
     public static void main(String[] args) {
         List<Integer> list = createInRangeArray(2, 10);
         printList(list);
@@ -32,12 +30,36 @@ public class MainApp {
         printList(list);
         increaseList(5, list);
         printList(list);
+
+        List<Employee> employeeList = new ArrayList<>();
+        Collections.addAll(employeeList,
+                new Employee("Дейв", 22),
+                new Employee("Боб", 21),
+                new Employee("Уилл", 25),
+                new Employee("Рик", 29),
+                new Employee("Джо", 35)
+        );
+        printList(employeeList);
+
+        List<Employee> trimedEmployeeList = Employee.trimEmployeeListByAge(employeeList, 25);
+        printList(trimedEmployeeList);
+
+        List<String> onlyNamesEmployeeList = Employee.returnOnlyNamesList(employeeList);
+        printList(onlyNamesEmployeeList);
+
+        System.out.println(Employee.isAgeAverageInList(employeeList, 30));
+        System.out.println(Employee.isAgeAverageInList(employeeList, 20));
+
+        Employee youngest = Employee.returnYoungest(employeeList);
+        System.out.println(youngest);
     }
 
-    public static void printList(List<Integer> list){
-        for (Integer integer : list) {
-            System.out.println(integer);
+    public static <T> void printList(List<T> list) {
+        System.out.println("=====");
+        for (T t : list) {
+            System.out.println(t);
         }
+        System.out.println("=====");
     }
 
     public static ArrayList<Integer> createInRangeArray(int min, int max) {
@@ -64,14 +86,14 @@ public class MainApp {
         return sum;
     }
 
-    public static void fillList(int in, List<Integer> list){
-        for (Integer integer : list){
+    public static void fillList(int in, List<Integer> list) {
+        for (Integer integer : list) {
             list.set(list.indexOf(integer), in);
         }
     }
 
-    public static void increaseList(int in, List<Integer> list){
-        for (Integer integer : list){
+    public static void increaseList(int in, List<Integer> list) {
+        for (Integer integer : list) {
             list.set(list.indexOf(integer), in + integer);
         }
     }
