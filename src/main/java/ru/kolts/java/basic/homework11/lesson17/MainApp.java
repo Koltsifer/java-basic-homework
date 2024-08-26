65e8ed45e91bf401dc2f65baea2223dbf1524217
Dmitriy JavaLearn <koltsifer@gmail.com>
2024-08-27 01:35:04 +0300

    homework11 started

---
+++ src/main/java/ru/kolts/java/basic/homework11/lesson17/MainApp.java
@@ -0,0 +1,14 @@
+package ru.kolts.java.basic.homework11.lesson17;
+
+/**
+ * Написать программу делающую следующее
+ * <p>
+ * ● Из Списка (List) сформировать двоичное дерево поиска
+ * <p>
+ * ● Написать рекурсивную функцию поиска в сформированном дереве
+ */
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
