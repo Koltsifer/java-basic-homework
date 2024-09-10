19470ecc31008eb524b5f0d4b5517d681b2b92ef
Dmitriy JavaLearn <koltsifer@gmail.com>
2024-09-10 14:29:22 +0300

    homework 12 complete

--- src/main/java/ru/kolts/java/basic/homework12/lesson19/javaio/MainApp.java
+++ src/main/java/ru/kolts/java/basic/homework12/lesson19/javaio/MainApp.java
@@ -1,7 +1,88 @@
 package ru.kolts.java.basic.homework12.lesson19.javaio;
 
+import java.io.File;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.Scanner;
+
+/**
+ * Реализуйте приложение, позволяющее работать с текстовыми файлами
+ * <p>
+ * При старте приложения, в консоль выводится список текстовых файлов из корневого каталоге проекта
+ * <p>
+ * Далее программа запрашивает имя файла, с которым хочет работать пользователь
+ * <p>
+ * Содержимое файла выводится в консоль
+ * <p>
+ * Затем любую введенную пользователем строку необходимо записывать в указанный файл
+ */
+
 public class MainApp {
     public static void main(String[] args) {
+        String path = "./";
+        printDir(path);
+        File file = findFile(path);
+        readFile(file);
+        writeFile(file);
+        readFile(file);
+    }
 
+    public static void printDir(String path){
+        if (path == null){
+            return;
+        }
+        File dir = new File(path);
+        if (dir.isDirectory()){
+            for (File f : Objects.requireNonNull(dir.listFiles())) {
+                System.out.println(f.getName());
+            }
+            System.out.println();
+        }
+    }
+
+    public static File findFile(String path){
+        if (path == null){
+            return null;
+        }
+        File file = null;
+        Scanner scanner = new Scanner(System.in);
+            System.out.println("Укажите имя файла:");
+            String fileName = scanner.nextLine();
+            for (File f : Objects.requireNonNull(new File(path).listFiles())) {
+                if (f.getName().equals(fileName)) {
+                    file = f;
+                }
+            }
+        return file;
+    }
+
+    public static void readFile(File file){
+        if (file == null){
+            return;
+        }
+        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(file))) {
+            int n = in.read();
+            while (n != -1) {
+                System.out.print((char) n);
+                n = in.read();
+            }
+            System.out.println();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeFile(File file){
+        if (file == null){
+            return;
+        }
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file, true))) {
+            System.out.println("Укажите данные для записи:");
+            byte[] buffer = new Scanner(System.in).nextLine().getBytes(StandardCharsets.UTF_8);
+            out.write(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
